179142760db45513473d8c6b9f2c558051399cf7
Eduardo Rosario <erosario@nodrix.com>
2017-10-25 21:12:29 -0400

    Exposed mongoose types to Anxeb

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ require('./source/middleware/prototypes')();
 
 const Server = require('./source/core/server');
 const Enums = require("./source/middleware/enums");
+const MongooseTypes = require('mongoose').Schema.Types;
 
 module.exports = {
 	Server : Server,
 	Enums  : Enums,
-	moment : require('moment')
+	Types  : {
+		ObjectId : MongooseTypes.ObjectId
+	}
 };
