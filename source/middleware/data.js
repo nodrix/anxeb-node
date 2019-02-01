849662d0a427056443b222b5a5e8b92358fe69a9
Eduardo Rosario <erosario@nodrix.com>
2019-01-31 21:50:23 -0400

    Data Model custom method argument hot fix

--- source/middleware/data.js
+++ source/middleware/data.js
@@ -47,9 +47,8 @@ module.exports = function (service) {
 							child_schema.method(m, child_params.methods[m]);
 
 							if (!Array.prototype[m]) {
-								Array.prototype[m] = function () {
-									var args = arguments.length ? arguments : undefined;
-									return this.map(function (value) { return value && value[m] ? value[m](args) : value; });
+								Array.prototype[m] = function (params) {
+									return this.map(function (value) { return value && value[m] ? value[m](params) : value; });
 								};
 							}
 						}
@@ -81,9 +80,8 @@ module.exports = function (service) {
 					schema.method(m, params.methods[m]);
 
 					if (!Array.prototype[m]) {
-						Array.prototype[m] = function () {
-							var args = arguments.length ? arguments : undefined;
-							return this.map(function (value) { return value && value[m] ? value[m](args) : value; });
+						Array.prototype[m] = function (params) {
+							return this.map(function (value) { return value && value[m] ? value[m](params) : value; });
 						};
 					}
 				}
