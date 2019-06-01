13a688d99de1448c5b4875a9f84698a60c0ccbef
Eduardo Rosario <erosario@nodrix.com>
2019-06-01 09:44:38 -0400

    Added array iterate prototype

--- source/client/prototypes.js
+++ source/client/prototypes.js
@@ -4,3 +4,31 @@ if (!String.prototype.replaceAll) {
 		return target.split(search).join(replacement);
 	}
 }
+
+if (!Array.prototype.iterate) {
+	Array.prototype.iterate = function (callback, filter) {
+		let items = this;
+
+		let process = function (item) {
+			if (filter == null || filter(item, i) === true) {
+				callback(item, i);
+			}
+		};
+
+		if (items instanceof Array) {
+			if (callback) {
+				for (let i in items) {
+					if (items.hasOwnProperty(i)) {
+						process(items[i]);
+					}
+				}
+			}
+		} else {
+			if (callback) {
+				for (let i = 0; i < items.length; i++) {
+					process(items[i]);
+				}
+			}
+		}
+	}
+}
